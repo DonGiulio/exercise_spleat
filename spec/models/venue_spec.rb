5e1739162d5c13bcb4eaf7a33cb45394bd44855c
Giulio Sante Giraldi <giulio@giulios-mbp.fritz.box>
2015-05-20 00:11:26 +0200

    first test case

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Venue, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @venues = create_list :venue, 10
+  end
+  
+  it "has several tabs" do
+    @venues.each do |v|
+      expect(v.tabs.size).to eq 10
+    end
+  end
+  
 end
