9e56986b936384e09de28529a1069eddf9b79301
Giulio Sante Giraldi <giulio@giulios-mbp.fritz.box>
2015-05-20 00:29:31 +0200

    venues have tabs

--- spec/factories/venues.rb
+++ spec/factories/venues.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :venue do
-    name "MyString"
+    sequence(:name) { |n| "venue_#{n}"}
+    
+    after :create do |v|
+      10.times.each {create :tab, :venue => v}
+    end
+    
   end
 
 end
